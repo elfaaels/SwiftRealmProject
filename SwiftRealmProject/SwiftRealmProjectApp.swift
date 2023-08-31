20e66c19eb22e85732c9368bc7a3dcaba51d9129
Elfana, A | Elfaael <elfaael12@gmail.com>
2023-08-31 20:12:12 +0700

    Initial Commit

---
+++ SwiftRealmProject/SwiftRealmProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRealmProjectApp.swift
+//  SwiftRealmProject
+//
+//  Created by Elfana Anamta Chatya on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRealmProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
